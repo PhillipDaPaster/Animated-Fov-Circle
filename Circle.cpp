7de66474126ee9d68c1c1fea275bf2437fd867c6
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-12-11 09:05:21 -0500

    Create Circle.cpp

---
+++ Circle.cpp
@@ -0,0 +1,23 @@
+void Anim_circle(float radius) {
+	ImGuiIO& io = ImGui::GetIO();
+	ImVec2 center(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.5f);
+	ImDrawList* drawList = ImGui::GetForegroundDrawList();
+
+	float timeFactor = fmod(ImGui::GetTime(), 5.0f) / 5.0f;
+
+	for (int i = 0; i < sides; ++i) {
+		ImVec2 pos(center.x, center.y);
+		ImU32 currentColor = ImGui::ColorConvertFloat4ToU32(ImColor::HSV((timeFactor - i / static_cast<float>(sides)) + 1.0f, 0.5f, 1.0f));
+
+		float angle = (i / static_cast<float>(sides)) * 2 * M_PI;
+		ImVec2 lastPos(pos.x + cos(angle) * radius, pos.y + sin(angle) * radius);
+		ImVec2 nextPos(pos.x + cos(angle + 2 * M_PI / sides) * radius, pos.y + sin(angle + 2 * M_PI / sides) * radius);
+
+		drawList->AddLine(lastPos, nextPos, IM_COL32(0, 0, 0, 255), 4.f);//outline
+
+		drawList->AddLine(lastPos, nextPos, currentColor, 2.f); //main
+	}
+}
+
+//example | пример
+Anim_circle(90.f);
