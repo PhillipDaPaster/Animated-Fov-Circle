0405340384c34e9f16b9e7a332fabcb6ed1ccf31
Phillip <49299203+PhillipThePaster@users.noreply.github.com>
2023-12-11 09:57:10 -0500

    Update Circle.cpp

--- Circle.cpp
+++ Circle.cpp
@@ -1,24 +1,25 @@
 #define M_PI 3.14159265358979323846
 
 void Anim_circle(float radius) {
-	ImGuiIO& io = ImGui::GetIO();
-	ImVec2 center(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.5f);
-	ImDrawList* drawList = ImGui::GetForegroundDrawList();
+    ImGuiIO& io = ImGui::GetIO();
+    ImVec2 center(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.5f);
+    ImDrawList* drawList = ImGui::GetForegroundDrawList(); // change this to GetBackgroundDrawlist to make it behind the menu
+     //  измените это на GetBackgroundDrawlist чтобы поместить его за меню
 
-	float timeFactor = fmod(ImGui::GetTime(), 5.0f) / 5.0f;
+    float timeFactor = fmod(ImGui::GetTime(), 5.0f) / 5.0f;
 
-	for (int i = 0; i < sides; ++i) {
-		ImVec2 pos(center.x, center.y);
-		ImU32 currentColor = ImGui::ColorConvertFloat4ToU32(ImColor::HSV((timeFactor - i / static_cast<float>(sides)) + 1.0f, 0.5f, 1.0f));
+    for (int i = 0; i < sides; ++i) {
+        ImVec2 pos(center.x, center.y);
+        ImU32 currentColor = ImGui::ColorConvertFloat4ToU32(ImColor::HSV((timeFactor - i / static_cast<float>(sides)) + 1.0f, 0.5f, 1.0f));
 
-		float angle = (i / static_cast<float>(sides)) * 2 * M_PI;
-		ImVec2 lastPos(pos.x + cos(angle) * radius, pos.y + sin(angle) * radius);
-		ImVec2 nextPos(pos.x + cos(angle + 2 * M_PI / sides) * radius, pos.y + sin(angle + 2 * M_PI / sides) * radius);
+        float angle = (i / static_cast<float>(sides)) * 2 * M_PI;
+        ImVec2 lastPos(pos.x + cos(angle) * radius, pos.y + sin(angle) * radius);
+        ImVec2 nextPos(pos.x + cos(angle + 2 * M_PI / sides) * radius, pos.y + sin(angle + 2 * M_PI / sides) * radius);
 
-		drawList->AddLine(lastPos, nextPos, IM_COL32(0, 0, 0, 255), 4.f);//outline | очертание
+        drawList->AddLine(lastPos, nextPos, IM_COL32(0, 0, 0, 255), 4.f);//outline | очертание
 
-		drawList->AddLine(lastPos, nextPos, currentColor, 2.f); //main | главный
-	}
+        drawList->AddLine(lastPos, nextPos, currentColor, 2.f); //main | главный
+    }
 }
 
 //example | пример
